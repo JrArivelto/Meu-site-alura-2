8cfd4de44db2460ef7938687b3323dd00955622a
JrArivelto <108800527+JrArivelto@users.noreply.github.com>
2024-06-10 11:26:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const CaixaPrincipal = document.querySelector(".caixa-principal")
+const CaixaPerguntas = document.querySelector(".caixas-perguntas")
+const CaixaAlternativas = document.querySelector(".caixas-alternativas")
+const CaixaResultado = document.querySelector(".caixa-resultado")
+const textoRessultado = document.querySelector("texto-resultado")
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]     
+    }
+]
+
+let atual = 0;
+let perguntaAtual
+
+function mostrarPergunta() {
+perguntaAtual = perguntas [atual]
+CaixaPerguntas.textContent = perguntaAtual[atual]
+
+}
